e2aa6211ae46d7613103945dcbcbe0961b717ac7
Tseng1114 <gray881114@gmail.com>
2025-10-01 19:58:56 +0800

    Fix order summary problem

---
+++ config.js
@@ -0,0 +1,6 @@
+;(function (w) {
+  w.APP_CONFIG = Object.freeze({
+    WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbwgZb848i7gFvkLkd0kjevWxfQta80wo8iHybs1Oyd5NYonWg-qHlSe1eZnhZAntCA/exec',
+    TOKEN_KEY: 'drinksys_token'
+  });
+})(window);

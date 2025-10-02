080253ab0427903c6743ccf86e473c52d9a110cc
Tseng1114 <gray881114@gmail.com>
2025-10-02 15:04:19 +0800

    Fixed an issue where order information displayed inconsistently across different devices

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 ;(function (w) {
   w.APP_CONFIG = Object.freeze({
-    WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbwgZb848i7gFvkLkd0kjevWxfQta80wo8iHybs1Oyd5NYonWg-qHlSe1eZnhZAntCA/exec',
+    WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbzMaUQ5RmwEp6AExNl4Jai-Jh8KZKI7tnMqlMfOdgSjumnmHta2iV5VzWr-8pRHT0c/exec',
     TOKEN_KEY: 'drinksys_token'
   });
 })(window);

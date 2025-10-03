75002d6a6bded97181b43537a1c4581c799184cc
Tseng1114 <gray881114@gmail.com>
2025-10-03 19:44:02 +0800

    Add line bot function

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 ;(function (w) {
   w.APP_CONFIG = Object.freeze({
-    WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbys1WHd7dUyR7PT0acv_VB6CtHV8QfraVo5A7iS6gNL7FGFIlJEF-NhX-lBtaiDYAw/exec',
+    WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbxggJYDJlZR5hNp--Db2CQB8fFReDYScjdOJcwcXFEdfUhCeCjrsJ1Sl5TRy4sb-SI/exec',
     TOKEN_KEY: 'drinksys_token'
   });
 })(window);

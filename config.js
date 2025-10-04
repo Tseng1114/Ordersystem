e9a35bf062aecd89591f5fcdca0cd83411b51498
Tseng1114 <gray881114@gmail.com>
2025-10-04 14:09:20 +0800

    Modify summary problem

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 ;(function (w) {
   w.APP_CONFIG = Object.freeze({
-    WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbycCJZUIPcyvQDCQE1JXOJrDzw7Px5u1nrtlvsQ9pmDJGR6JF3rnyzQahL5NaL1uOI/exec',
+    WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbzuTzUs1DU49vUnRPup0rN-10BtVRbSzxH0EGbkm9v_fcdWYX42-9rMNpzp965-r-U/exec',
     TOKEN_KEY: 'drinksys_token'
   });
 })(window);

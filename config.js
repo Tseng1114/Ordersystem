ea36a9af1acfc10636bdf08e74cf50fa703f5b61
Tseng1114 <gray881114@gmail.com>
2025-10-05 00:47:34 +0800

    Update line bot

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 ;(function (w) {
   w.APP_CONFIG = Object.freeze({
-    WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbzuTzUs1DU49vUnRPup0rN-10BtVRbSzxH0EGbkm9v_fcdWYX42-9rMNpzp965-r-U/exec',
+    WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbz5N6A5NdIsbV9Ovz7-bThLrFp-907j0wMJs6O-1rdbuVcYgIz_YsmbZXY-jUsvA9o/exec',
     TOKEN_KEY: 'drinksys_token'
   });
 })(window);

3b448c1055e10100a596d843c5a3f5bbe8fd069c
Tseng1114 <gray881114@gmail.com>
2025-10-04 13:09:31 +0800

    Modify linebot problem about sugar content display problem

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 ;(function (w) {
   w.APP_CONFIG = Object.freeze({
-    WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbxggJYDJlZR5hNp--Db2CQB8fFReDYScjdOJcwcXFEdfUhCeCjrsJ1Sl5TRy4sb-SI/exec',
+    WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbzyR_lidvQBPiL6izPeC6t3St5dyxgy0pod9cOsXequMOMb0h3bf2y1KnaFEME0Y28/exec',
     TOKEN_KEY: 'drinksys_token'
   });
 })(window);

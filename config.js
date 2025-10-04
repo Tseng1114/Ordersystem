e6a0d7d3795b97fedeadfc0c8350cafc754ebab6
Tseng1114 <gray881114@gmail.com>
2025-10-04 13:17:02 +0800

    Modify config

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 ;(function (w) {
   w.APP_CONFIG = Object.freeze({
-    WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbzyR_lidvQBPiL6izPeC6t3St5dyxgy0pod9cOsXequMOMb0h3bf2y1KnaFEME0Y28/exec',
+    WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbycCJZUIPcyvQDCQE1JXOJrDzw7Px5u1nrtlvsQ9pmDJGR6JF3rnyzQahL5NaL1uOI/exec',
     TOKEN_KEY: 'drinksys_token'
   });
 })(window);

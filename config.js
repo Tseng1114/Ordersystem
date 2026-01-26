217f7d68766bf2aef09475f2ec76aa496bd3e59c
Tseng1114 <gray881114@gmail.com>
2026-01-26 16:26:02 +0800

    excel to supabase

--- config.js
+++ config.js
@@ -1,6 +1,13 @@
+/*
 ;(function (w) {
   w.APP_CONFIG = Object.freeze({
     WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbz5N6A5NdIsbV9Ovz7-bThLrFp-907j0wMJs6O-1rdbuVcYgIz_YsmbZXY-jUsvA9o/exec',
     TOKEN_KEY: 'drinksys_token'
   });
 })(window);
+*/
+// app script to supabase
+window.APP_CONFIG = {
+  SUPABASE_URL: 'https://eyahdrptpwegfreaqacw.supabase.co',
+  SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV5YWhkcnB0cHdlZ2ZyZWFxYWN3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk0MDY0NjksImV4cCI6MjA4NDk4MjQ2OX0.7ZPY035-_RbQGqLTpKzwC-Vz9HdlDFxYemeCAAYC1aM',
+};

84b2cc4ce02bab3f1868ce6434e75c5e6020aa78
Tseng1114 <gray881114@gmail.com>
2026-02-11 20:47:49 +0800

    modify vite config

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(supabaseUrl, supabaseKey);
--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,33 @@
-import { defineConfig } from 'vite'
-import { resolve } from 'path'
+import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
-  base: './',
+  base: "./",
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        create_event: resolve(__dirname, 'create_event.html'),
-        order: resolve(__dirname, 'order.html'),
-        summary: resolve(__dirname, 'summary.html')
-      }
-    }
-  }
-})
+        main: resolve(__dirname, "index.html"),
+        create_event: resolve(__dirname, "create_event.html"),
+        order: resolve(__dirname, "order.html"),
+        summary: resolve(__dirname, "summary.html"),
+        "8yotea": resolve(__dirname, "all_html/8yotea.html"),
+        "50lan": resolve(__dirname, "all_html/50lan.html"),
+        aniceholiday: resolve(__dirname, "all_html/aniceholiday.html"),
+        bogteashop: resolve(__dirname, "all_html/bogteashop.html"),
+        chingshing: resolve(__dirname, "all_html/chingshing.html"),
+        damingtea: resolve(__dirname, "all_html/damingtea.html"),
+        dejeng: resolve(__dirname, "all_html/dejeng.html"),
+        kebuke: resolve(__dirname, "all_html/kebuke.html"),
+        lairitea: resolve(__dirname, "all_html/lairitea.html"),
+        liketea: resolve(__dirname, "all_html/liketea.html"),
+        macutea: resolve(__dirname, "all_html/macutea.html"),
+        milksha: resolve(__dirname, "all_html/milksha.html"),
+        minimini: resolve(__dirname, "all_html/minimini.html"),
+        peaktea: resolve(__dirname, "all_html/peaktea.html"),
+        tptea: resolve(__dirname, "all_html/tptea.html"),
+        truedan: resolve(__dirname, "all_html/truedan.html"),
+        youdrink: resolve(__dirname, "all_html/youdrink.html"),
+      },
+    },
+  },
+});

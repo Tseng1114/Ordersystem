a428bfcd4e6dfd1bc0129e52d519556384e20fec
Tseng1114 <gray881114@gmail.com>
2026-02-11 20:34:55 +0800

    add github actions workflow for vite deployment

--- config.js
+++ config.js
@@ -1,12 +1,6 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || "https://eyahdrptpwegfreaqacw.supabase.co"
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV5YWhkcnB0cHdlZ2ZyZWFxYWN3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk0MDY0NjksImV4cCI6MjA4NDk4MjQ2OX0.7ZPY035-_RbQGqLTpKzwC-Vz9HdlDFxYemeCAAYC1aM"
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
-
-console.log('Supabase 已初始化:', supabase)
-
-/*
-beta version
-*/
